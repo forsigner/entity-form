c72b1c693e3305bbfcd5be14d93028435802ed05
forsigner <forsigner@gmail.com>
2020-05-10 11:39:48 +0800

    chore: update example

--- example/entity-form/formContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react'
-import { Result } from './types'
-
-export const formContext = createContext({} as Result)
--- example/entity-form/stores/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './entityStore'
-export * from './fieldStore'
