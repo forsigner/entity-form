07d93c75de1f3b11098903e9ae2f1783964be4be
forsigner <forsigner@gmail.com>
2020-05-13 21:45:22 +0800

    example: rm no use code

--- example/entity-form-antd/fields/Form.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from 'react'
-import { RegisterFormProps } from '../../entity-form/types'
-
-export const Form: FC<RegisterFormProps> = props => {
-  return <form {...props}>{props.children}</form>
-}
