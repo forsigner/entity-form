7665567a89e37fef3fa10b10058a2baa29238fd0
forsigner <forsigner@gmail.com>
2020-06-11 17:58:00 +0800

    docs: improve docs

---
+++ website/src/components/QuickStartExample.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// import { EntityForm, entity, field } from 'entity-form'
+
+// @entity('user-entity')
+// class User {
+//   @field({ value: '', component: 'Input' })
+//   username: string
+
+//   @field({ value: '', component: 'Input', type = 'passord' })
+//   password: string
+
+//   onSubmit(values: User) {
+//     alert(JSON.stringify(values, null, 2))
+//   }
+// }
+
+export default () => {
+  return (
+    <div>
+      <div>....QuickStartExample</div>
+      <div>....QuickStartExample</div>
+      {/* <EntityForm entity={User}></EntityForm> */}
+    </div>
+  )
+}
